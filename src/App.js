dd78b49124f787f2e1196dd9f622c0a85c31d461
Muhammad Ismail <ismardoi2001@gmail.com>
2020-06-20 09:54:41 +0500

    update

--- src/App.js
+++ src/App.js
@@ -4,13 +4,20 @@ import './App.css';
 export default function App() {
 
 let [count, setCount] = useState(1);
-return(
-  <div>
+let [isMorning,setMorning] = useState(false)
+return (
+  <div className={'box ${isMorning ? 'dayLight':''}'}>
+  <h1>Day Time = {isMorning ? 'Morning' : 'Night'}</h1>
+
   <Message counter={count}/>
   <br />
   <button onClick={()=> setCount(count +1)}>
   Update Counter
   </button>
+  <button onClick={() => setMorning(isMorning)}> 
+  Update Day
+  </button>
   </div>
+
 )
 };
